a5e269dde2df1c9dd3e332246e1949befda54fd7
uwuusowarm <pe.be210997@gmail.com>
2025-04-08 15:08:44 +0200

    +Finished Prototype Card Template

--- Assets/3_Scripts/CardTemplate.cs
+++ Assets/3_Scripts/CardTemplate.cs
@@ -1,69 +1,58 @@
 using System;
+using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class CardTemplate : MonoBehaviour
 {
-    [Header("Class")]
-    public String cardClass;
+    [Header("Class")] public String cardClass;
+
+    [Header("Card Name")] public String cardName;
+
+    [Header("Left Value")] [Range(0, 10)] public int leftVal = 1;
+
+    [Header("Right Value")] [Range(0, 10)] public int rightVal = 1;
+
+    [Header("Range Value")] [Range(1, 3)] public int rangeVal = 1;
+
+    [Header("Effect")] public String effectName;
+
+    [Header("Illustration")] public Sprite illustrationBig;
     
-    [Header("Card Name")]
-    public String cardName;
-    
-    [Header("Attack Value")]
-    [Range(0, 10)]
-    public int attackVal = 1;  
-    
-    [Header("Block Value")]
-    [Range(0, 10)]
-    public int blockVal = 1; 
-    
-    [Header("Range Value")]
-    [Range(0, 5)]
-    public int rangeVal = 1; 
-    
-    private int tempAt;
-    private int tempBlock;
+    [Header("Icons")] public Sprite iconLeft; public Sprite iconRight;
+
+    private TextMeshProUGUI valLeftText;
+    private TextMeshProUGUI valRightText;
+    private TextMeshProUGUI effectNameText;
+    private TextMeshProUGUI cardNameText;
+    private Image leftIconImage;
+    private Image rightIconImage;
+    private Image illustrationImage;
+
+
+    void Awake()
+    {
+        valLeftText = transform.Find("valLeft").GetComponent<TextMeshProUGUI>();
+        valRightText = transform.Find("valRight").GetComponent<TextMeshProUGUI>();
+        effectNameText = transform.Find("effect").GetComponent<TextMeshProUGUI>();
+        leftIconImage = transform.Find("leftIcon").GetComponent<Image>();
+        rightIconImage = transform.Find("rightIcon").GetComponent<Image>();
+        illustrationImage = transform.Find("illustration").GetComponent<Image>();
+        cardNameText = transform.Find("name").GetComponent<TextMeshProUGUI>();
+    }
     void Start()
     {
-        tempAt = attackVal;
-        tempBlock = blockVal;
-        
-        if (cardClass == null)
-        {
-            Debug.Log("Card Class not set");
-        }
-        else
-        {
-            Debug.Log("Class: " + cardClass);
-        }
-
-        if (cardName == null)
-        {
-            Debug.Log("Card Name not set");
-        }
-        else
-        {
-            Debug.Log("Card Name not set");
-        }
-        
-        Debug.Log("Attack: " + attackVal + " Block: " + blockVal + " Range: " + rangeVal);
-
-        
+        if (valLeftText != null) valLeftText.text = leftVal.ToString();
+        if (valRightText != null) valRightText.text = rightVal.ToString();
+        if (effectNameText != null) effectNameText.text = effectName;
+        if (leftIconImage != null) leftIconImage.sprite = iconLeft;
+        if (rightIconImage != null) rightIconImage.sprite = iconRight;
+        if (illustrationImage != null) illustrationImage.sprite = illustrationBig;
+        if (cardNameText != null) cardNameText.text = cardName;
     }
 
     void Update()
     {
-        if (tempAt != attackVal)
-        {
-            Debug.Log("New Attack Value: " + attackVal);
-            tempAt = attackVal;
-        }
 
-        if (tempBlock != blockVal)
-        {
-            Debug.Log("New Block Value: " + blockVal);
-            tempBlock = blockVal;
-        }
     }
 }
