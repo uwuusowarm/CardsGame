3b46f5e734c40ca8490d6a625d110e52005d4fde
TheLord65 <adrian.hein@games-academy.de>
2025-04-08 15:17:48 +0200

    Main_Manu001

---
+++ Assets/3_Scripts/Main_Manu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Manu : MonoBehaviour
+{
+  public void startGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OptionGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CreditsGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void DeckGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
+
