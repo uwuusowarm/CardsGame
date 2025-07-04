77de172851c7be4a09e42fb48c6ed2391cf7e9c8
Elija <eli.schmuck@gmx.net>
2025-07-04 03:31:49 +0200

    Card Menu (unfinisthed, 2 mental breakdowns) v.0.1

--- Assets/3_Scripts/MainMenu/CardSelectorUI.cs
+++ Assets/3_Scripts/MainMenu/CardSelectorUI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 [RequireComponent(typeof(Button))]
 public class CardSelectorUI : MonoBehaviour
@@ -10,7 +11,6 @@ public class CardSelectorUI : MonoBehaviour
     private CardData assignedCard;
     private CardMenuManager manager;
 
-    // Die Methode empfängt das visuelle Prefab jetzt als neuen Parameter
     public void Initialize(CardData card, GameObject visualPrefab, CardMenuManager menuManager, bool isSelected)
     {
         this.assignedCard = card;
@@ -21,28 +21,26 @@ public class CardSelectorUI : MonoBehaviour
             Destroy(child.gameObject);
         }
 
-        // Benutze das übergebene Prefab
         if (cardParent != null && visualPrefab != null)
         {
             GameObject visualCardInstance = Instantiate(visualPrefab, cardParent);
 
-            CardDragHandler dragHandler = visualCardInstance.GetComponentInChildren<CardDragHandler>(true);
-            if (dragHandler != null)
+            TextMeshProUGUI[] texts = visualCardInstance.GetComponentsInChildren<TextMeshProUGUI>(true);
+            foreach (var text in texts)
             {
-                Destroy(dragHandler);
+                Material editableMaterial = new Material(text.fontMaterial);
+                editableMaterial.EnableKeyword("MASK_HARD");
+                text.fontMaterial = editableMaterial;
             }
+
+            CardDragHandler dragHandler = visualCardInstance.GetComponentInChildren<CardDragHandler>(true);
+            if (dragHandler != null) Destroy(dragHandler);
 
             CanvasGroup canvasGroup = visualCardInstance.GetComponentInChildren<CanvasGroup>(true);
-            if (canvasGroup != null)
-            {
-                canvasGroup.blocksRaycasts = false;
-            }
+            if (canvasGroup != null) canvasGroup.blocksRaycasts = false;
 
             Graphic[] graphics = visualCardInstance.GetComponentsInChildren<Graphic>(true);
-            foreach (Graphic g in graphics)
-            {
-                g.raycastTarget = false;
-            }
+            foreach (Graphic g in graphics) g.raycastTarget = false;
 
             ScaleCardToFit(visualCardInstance, cardParent);
         }
@@ -57,22 +55,15 @@ public class CardSelectorUI : MonoBehaviour
     {
         RectTransform cardRect = cardInstance.GetComponent<RectTransform>();
         RectTransform parentRect = parentContainer.GetComponent<RectTransform>();
-
         if (cardRect == null || parentRect == null) return;
-
         LayoutRebuilder.ForceRebuildLayoutImmediate(parentRect);
-
         cardRect.anchorMin = cardRect.anchorMax = cardRect.pivot = new Vector2(0.5f, 0.5f);
-
         float cardWidth = cardRect.rect.width;
         float cardHeight = cardRect.rect.height;
         float parentWidth = parentRect.rect.width;
         float parentHeight = parentRect.rect.height;
-
         if (cardWidth == 0 || cardHeight == 0) return;
-
         float scaleRatio = Mathf.Min(parentWidth / cardWidth, parentHeight / cardHeight);
-
         cardRect.localScale = new Vector3(scaleRatio, scaleRatio, 1f);
         cardRect.anchoredPosition = Vector2.zero;
     }
